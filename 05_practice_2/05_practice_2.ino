42e09a62a7d306cc5186c0c8662c480921d0b46f
김우현 <mrpc2003@gmail.com>
2022-09-15 16:04:21 +0900

    09/15 upload

---
+++ 05_practice_2/05_practice_2.ino
@@ -0,0 +1,20 @@
+int LED_07 = 7;
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_07, OUTPUT);
+  
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_07, LOW);   // turn the LED off ==== +/- changed
+  delay(1000);                       // wait for a second
+  for (int i = 0; i < 5; i++) {
+  digitalWrite(LED_07, HIGH);
+  delay(100);
+  digitalWrite(LED_07, LOW);
+  delay(100);
+  }
+  digitalWrite(LED_07, HIGH);
+  while(1){}
+}
